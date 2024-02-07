d1165a31452b49d25355f603714e10e9b4d4baa0
selasiii <52832049+selasiii@users.noreply.github.com>
2024-02-07 15:40:30 +0000

    models and auth controllers for admin and agents

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,29 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
 //use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles;
 
     protected $fillable = [
-        'customer_id',
+        'admin_id',
+        'role_id',
         'first_name',
         'last_name',
         'email',
         'email_verified_at',
-        'address',
-        'phone_number',
         'password',
-        'gender',
-        'date_of_birth',
     ];
+
 
     protected $hidden = [
         'password',
@@ -32,8 +29,8 @@ class User extends Authenticatable
     ];
 
 
+
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'date_of_birth' => 'date',
     ];
 }
--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -2,19 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
 //use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class Agent extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles;
 
     protected $fillable = [
-        'customer_id',
+        'agent_id',
+        'role_id',
         'first_name',
         'last_name',
         'email',
@@ -30,6 +30,7 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
 
 
     protected $casts = [
